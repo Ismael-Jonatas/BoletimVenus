95c76d8a0ac003587a5bf7bef32ee65ca292bd8f
ismael jonatas <jhons1808@outlook.com>
2021-11-10 20:50:37 -0300

    Estrutra MVC SpringBoot

---
+++ BoletimVenus/src/main/java/br/edu/ifpb/pweb2/BoletimVenusSB/BoletimVenus/BoletimVenusApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.BoletimVenusSB.BoletimVenus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletimVenusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletimVenusApplication.class, args);
+	}
+
+}
---
+++ BoletimVenus/src/test/java/br/edu/ifpb/pweb2/BoletimVenusSB/BoletimVenus/BoletimVenusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.BoletimVenusSB.BoletimVenus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletimVenusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
