b998fcc1c534e6e64fe0f77a343169e9c3d8bcd0
WillmaTayanne <willmatayanne@gmail.com>
2021-11-29 22:23:23 -0300

    Adicionando redirecionamento tela cadastro

---
+++ BoletimVenus/src/main/java/br/edu/ifpb/pweb2/BoletimVenusSB/BoletimVenus/Controller/CadastroController.java
@@ -0,0 +1,28 @@
+package br.edu.ifpb.pweb2.BoletimVenusSB.BoletimVenus.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.edu.ifpb.pweb2.BoletimVenusSB.BoletimVenus.Model.Estudante;
+import br.edu.ifpb.pweb2.BoletimVenusSB.BoletimVenus.Service.EstudanteService;
+
+@Controller
+@RequestMapping("/cadastro")
+
+public class CadastroController {
+
+	@Autowired
+	private EstudanteService estudanteservice;
+	
+	
+	@RequestMapping
+	public String getFromLogin(Model model) {
+		model.addAttribute("estudante", new Estudante());
+		return "/cadastro/form-cadastro";
+	}
+	
+	
+	
+}
